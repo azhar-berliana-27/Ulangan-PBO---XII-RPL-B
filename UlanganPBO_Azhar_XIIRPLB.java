e020edee75ee2ee2ec731aca47035a45ee562842
Azhar Berliana Putra Santosa <71066159+azhar-berliana-27@users.noreply.github.com>
2020-09-10 21:06:52 +0700

    Add files via upload
    Bismillah dapet pulsa 50k aamiin... hehe :v

---
+++ UlanganPBO_Azhar_XIIRPLB.java
@@ -0,0 +1,72 @@
+package ulanganpbo_azhar_xiirplb;
+/**
+ * Azhar Berliana Putra. S
+ * XII - RPL - B
+ * @author Krypton27
+ */
+public class UlanganPBO_Azhar_XIIRPLB {
+
+    public static void main(String[] args) {
+        
+        //Membuat daftar alfabet dengan String
+        String a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+        //---------------------------------------------------------------------
+        
+        //Memanggil alfabet String yang diinginkan
+        //Menentukan alfabet untuk Nama
+        char hasil1 = a.charAt(0);
+        char hasil2 = a.charAt(25);
+        char hasil3 = a.charAt(7);
+        char hasil4 = a.charAt(0);
+        char hasil5 = a.charAt(17);
+        char hasil6 = a.charAt(27);
+        char hasil7 = a.charAt(1);
+        char hasil8 = a.charAt(4);
+        char hasil9 = a.charAt(17);
+        char hasil10 = a.charAt(11);
+        char hasil11 = a.charAt(8);
+        char hasil12 = a.charAt(0);
+        char hasil13 = a.charAt(13);
+        char hasil14 = a.charAt(0);
+        //---------------------------------------------------------------------
+        
+        //Menentukan alfabet untuk Kelas
+        char hasil15 = a.charAt(23);
+        char hasil16 = a.charAt(8);
+        char hasil17 = a.charAt(8);
+        char hasil18 = a.charAt(27);
+        char hasil19 = a.charAt(17);
+        char hasil20 = a.charAt(15);
+        char hasil21 = a.charAt(11);
+        char hasil22 = a.charAt(27);
+        char hasil23 = a.charAt(1);
+        //---------------------------------------------------------------------
+        
+        //Saat nya memanggil output yang telah di tentukan pada variable-variable diatas
+        System.out.println("ALFABET TERSEDIA : "+a);
+        //---------------------------------------------------------------------
+        
+        //---------------------------------------------------------------------
+        System.out.println("---------------------------------------------------"
+                + "------------------");
+        //---------------------------------------------------------------------
+        
+        //Pemanggilan Nama
+        System.out.println("Nama  : "+hasil1+""+hasil2+""+hasil3+""+hasil4+
+                ""+hasil5+""+hasil6+""+hasil7+""+hasil8+""+hasil9+""+hasil10+""
+                +hasil11+""+hasil12+""+hasil13+""+hasil14);
+        //---------------------------------------------------------------------
+        
+        //Pemanggilan Kelas
+        System.out.println("Kelas : "+hasil15+""+hasil16+""+hasil17+""+
+                hasil18+""+hasil19+""+hasil20+""+hasil21+""+hasil22+""+hasil23);
+        //---------------------------------------------------------------------
+        
+        //---------------------------------------------------------------------
+        System.out.println("---------------------------------------------------"
+                + "------------------");
+        //---------------------------------------------------------------------
+        
+    }
+    
+}
